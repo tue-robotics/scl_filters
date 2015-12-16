dfca8d0d94dde48ab5b8e28e3e1d4c9a628ba6fa
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2015-12-16 11:41:58 +0100

    Added test

---
+++ test/test_filters.cpp
@@ -0,0 +1,165 @@
+#include <scl/filters/DFirstOrderLowpass.hpp>
+#include <scl/filters/DSecondOrderLowpass.hpp>
+#include <scl/filters/DLeadLag.hpp>
+#include <scl/filters/DPD.hpp>
+#include <scl/filters/DPID.hpp>
+#include <scl/filters/DSkewedNotch.hpp>
+#include <scl/filters/DWeakIntegrator.hpp>
+
+#include <cmath>
+#include <string>
+#include <iostream>
+#include <vector>
+
+// ----------------------------------------------------------------------------------------------------
+
+double runFilter(DFILTERS::Filter& filter)
+{
+    for(unsigned int i = 0; i < 1000; ++i)
+    {
+        double input = sin(0.001 * i);
+        filter.update(input);
+    }
+
+    return filter.getOutput();
+}
+
+// ----------------------------------------------------------------------------------------------------
+
+bool testFilter(const std::string& name, DFILTERS::Filter& filter, double expected_outcome)
+{
+    double output = runFilter(filter);
+
+    bool ok = std::abs(filter.getOutput() - expected_outcome) < 1e-5;
+    if (ok)
+        std::cout << "[OK]   ";
+    else
+        std::cout << "[ERROR]";
+
+
+    std::cout << " " << name;
+
+    if (!ok)
+    {
+        std::cout.precision(10);
+        std::cout << "    (outcome = " << filter.getOutput() << ", expected = " << expected_outcome << ")";
+    }
+    std::cout << std::endl;
+
+    return ok;
+}
+
+// ----------------------------------------------------------------------------------------------------
+
+struct FilterInfo
+{
+    FilterInfo(const std::string& name_, DFILTERS::Filter* filter_) : name(name_), filter(filter_) {}
+    std::string name;
+    DFILTERS::Filter* filter;
+};
+
+// ----------------------------------------------------------------------------------------------------
+
+bool testFilters(std::vector<FilterInfo>& filters, const double* outcomes, int num_methods)
+{
+    bool ok = true;
+
+    int num_filters = filters.size() / num_methods;
+
+    for(unsigned int i = 0; i < filters.size(); ++i)
+    {
+        if (i % num_filters == 0)
+        {
+            std::cout << std::endl;
+            std::cout << "--------------------------------------------" << std::endl;
+            std::cout << " Discretization method " << (i / num_filters + 1) << std::endl;
+            std::cout << "--------------------------------------------" << std::endl;
+            std::cout << std::endl;
+        }
+
+        FilterInfo& info = filters[i];
+        ok &= testFilter(info.name, *info.filter, outcomes[i]);
+    }
+
+    return ok;
+}
+
+// ----------------------------------------------------------------------------------------------------
+
+void printFilterOutcomes(std::vector<FilterInfo>& filters)
+{
+    std::cout.precision(8);
+    std::cout << "double OUTCOMES[] = {";
+
+    for(unsigned int i = 0; i < filters.size(); ++i)
+    {
+        FilterInfo& info = filters[i];
+        double output = runFilter(*info.filter);
+        if (output != output)
+            output = 0;
+
+        if (i % 6 == 0)
+            std::cout << std::endl << "    ";
+
+        std::cout << output;
+
+        if (i + 1 != filters.size())
+            std::cout << ", ";
+    }
+    std::cout << std::endl << "    };" << std::endl;
+}
+
+// ----------------------------------------------------------------------------------------------------
+
+int main(int argc, char **argv)
+{
+    std::vector<FilterInfo> filters;
+
+    double dt = 0.001;
+
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    // Expected outcomes
+
+    // Use printFilterOutcomes below to generate these values
+    double OUTCOMES[] = {
+        0.84006621, 0.83484567, 0.89357953, 34.34025, 19.296261, 0.84309577,
+        0.92750192, 0.84006755, 0.83485498, 0.89349839, 34.290459, 19.292079,
+        0.84309244, 0.92734341, 0.84006688, 0.83485032, 0.89353898, 34.315363,
+        19.29417, 0.84309411, 0.92742267, 0.84009554, 0.83485839, 0.89352229,
+        34.315363, 19.294171, 0.8430683, 0.92742267, 0.83976732, 0.84093026,
+        0.90408014, 35.991763, 19.348789, 0.84093026, 0.92734341, 0.83976732,
+        0.84093026, 1.1818667, 34.31577, 0.84093026, 0.84093026, 0.92733527
+        };
+
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    // Initialize filters
+
+    for(int method = 1; method <= 6; ++method)
+    {
+        filters.push_back(FilterInfo("DFirstOrderLowpass", new DFILTERS::DFirstOrderLowpass(100, dt, method)));
+        filters.push_back(FilterInfo("DSecondOrderLowpass", new DFILTERS::DSecondOrderLowpass(20, 0.7, dt, method)));
+        filters.push_back(FilterInfo("DLeadLag", new DFILTERS::DLeadLag(1.6, 60, dt, method)));
+        filters.push_back(FilterInfo("DPD", new DFILTERS::DPD(1.6, 60, dt, method)));
+        filters.push_back(FilterInfo("DPID", new DFILTERS::DPID(20, 2, 3, dt, method)));
+        filters.push_back(FilterInfo("DSkewedNotch", new DFILTERS::DSkewedNotch(60, 1.5, 80, 1.0, dt, method)));
+        filters.push_back(FilterInfo("DWeakIntegrator", new DFILTERS::DWeakIntegrator(0.03, dt, method)));
+    }
+
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    // Use this to generate the outcome array above
+
+//    printFilterOutcomes(filters);
+
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    // Test the filters
+
+    if (!testFilters(filters, OUTCOMES, 6))
+    {
+        std::cout << std::endl << "[ERROR]" << std::endl;
+        return 1;
+    }
+
+    std::cout << std::endl << "[OK]" << std::endl;
+
+    return 0;
+}
